b547b77687be86764c1fd01665b622692c2fa7b1
neeraj15022001 <gneeraj32595@gmail.com>
2021-07-09 19:37:26 +0530

    merge sort in progress

--- MergeSortLinkedList.java
+++ MergeSortLinkedList.java
@@ -1,35 +1,89 @@
-public class MergeSortLinkedList {
-    public static LinkedListPractice llp = new LinkedListPractice();
+import java.util.Scanner;
 
-    public Node<Integer> getMid(Node<Integer> head) {
-        int length = llp.getLinkedListLength(head);
-        int mid = (length - 1)/2;
-        int i = 0;
-        while (i != mid) {
+class MergeSortLinkedList {
+
+    public static Node<Integer> getTail(Node<Integer> head) {
+        Node<Integer> tail = head;
+        while (tail.nextNode != null) {
+            tail = tail.nextNode;
+        }
+        return tail;
+    }
+
+    public static Node<Integer> merge(Node<Integer> list1, Node<Integer> list2) {
+        Node<Integer> result = null;
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.data <= list2.data) {
+            result = list1;
+            result.nextNode = merge(list1.nextNode, list2);
+        } else {
+            result = list2;
+            result.nextNode = merge(list1, list2.nextNode);
+        }
+        return result;
+    }
+
+    public static Node<Integer> mergeSort(Node<Integer> head) {
+        if (head == null || head.nextNode == null)
+            return head;
+        Node<Integer> mid = findMid(head, tail);
+        System.out.println(mid.data);
+        Node<Integer> list1 = mergeSort(head, mid);
+        Node<Integer> list2 = mergeSort(mid.nextNode, tail);
+        Node<Integer> sortedList = merge(list1, list2);
+        return sortedList;
+    }
+
+    public static Node<Integer> findMid(Node<Integer> head, Node<Integer> tail) {
+        int count = 0;
+        Node<Integer> mid = head;
+        while (head != tail) {
+            if ((count % 2) == 1)
+                mid = mid.nextNode;
+            ++count;
             head = head.nextNode;
-            i++;
+        }
+        if (mid != null)
+            return mid;
+        else
+            throw new ArithmeticException("List size is less");
+    }
+
+    public static Node<Integer> takeInput() {
+        Node<Integer> head = null;
+        Node<Integer> tail = null;
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        while (data != -1) {
+            Node<Integer> newNode = new Node<>(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.nextNode = newNode;
+                tail = tail.nextNode;
+            }
+            data = sc.nextInt();
         }
         return head;
     }
 
-    public void mergeSort(Node<Integer> head, Node<Integer> tail) {
-        int length = llp.getLinkedListLength(head);
-        MergingLL mll = new MergingLL();
-        if (length <= 1) {
-            return;
+    public static void printLinkedList(Node<Integer> head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.nextNode;
         }
-        Node<Integer> mid = getMid(head);
-        mergeSort(head, mid);
-        mergeSort(mid.nextNode, tail);
-        mll.merge(head, tail);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        System.out.println(llp);
-        Node<Integer> head = llp.takeInput();
-        Node<Integer> tail = llp.getTail(head);
-        MergeSortLinkedList msll = new MergeSortLinkedList();
-        msll.mergeSort(head, tail);
-        llp.printLinkedList(head);
+        Node<Integer> head = takeInput();
+        printLinkedList(head);
+        Node<Integer> tail = getTail(head);
+        Node<Integer> mid = findMid(head, tail);
+        mergeSort(head, tail);
     }
 }
